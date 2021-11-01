9de609b4cc31518b98f285b6655e57961b4cdd35
Lakshya-Agiwal <85127186+Lakshya-Agiwal@users.noreply.github.com>
2021-11-01 20:45:57 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -10,6 +10,21 @@ recognition.onresult=function(event){
     console.log(event);
     content=event.results[0][0].transcript;
     document.getElementById("textbox").innerHTML = content;
+    speak();
 }
+function speak()
+{
+    var synth = window.speechSynthesis;
+    var speak_data = document.getElementById("textbox").value;
+    var utter_this = new SpeechSynthesisUtterance(speak_data);
+    synth.speak(utter_this);
+    Webcam.attach('#camera');
+}
+ Webcam.set({
+     width:360,
+     height:360,
+     image_format:'png',
+     png_quality:90
+    })
 
 
