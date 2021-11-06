57fc1b86aa7b2db1ed1ae61050ee86952786f2af
Lakshya-Agiwal <85127186+Lakshya-Agiwal@users.noreply.github.com>
2021-11-06 11:56:22 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -10,21 +10,49 @@ recognition.onresult=function(event){
     console.log(event);
     content=event.results[0][0].transcript;
     document.getElementById("textbox").innerHTML = content;
-    speak();
+    if (content=="take my selfie")
+    {
+        speak();
+        setTimeout(function (){
+            snapshot();
+            save(); 
+        },5000);
+        
+    }
+    
 }
 function speak()
 {
+    
     var synth = window.speechSynthesis;
-    var speak_data = document.getElementById("textbox").value;
+    var speak_data = "Taking your selfie in 5 seconds";
     var utter_this = new SpeechSynthesisUtterance(speak_data);
     synth.speak(utter_this);
     Webcam.attach('#camera');
 }
+
  Webcam.set({
      width:360,
      height:360,
      image_format:'png',
      png_quality:90
     })
+    function snapshot()
+    {
+        Webcam.snap(function(data_uri){
+            
+         document.getElementById("selfie").innerHTML = "<img src="+data_uri+" id='selfie'>" ;
+        })
+        
+    }  
+
+    function save()
+    {
+        link = document.getElementById("link");
+        image = document.getElementById("selfie").src;
+        link.href=image;
+        link.click();
+    }
+    
 
 
